425148cdc4b1c1ca356dd1ad279e8c6124f10305
EchoingPeng <31258792+EchoingPeng@users.noreply.github.com>
2019-09-25 21:34:23 -0400

    First commit
    Crawlers for suspended stock list and news articles.

---
+++ news_crawler.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import datetime
+
+news_list = []
+
+## Article content class
+class Content:
+
+    ## base class for all articles
+    def __init__(self, keyword, url, title, body):
+        self.keyword = keyword
+        self.url = url
+        self.title = title
+        self.body = body
+        
+
+    ## function to control output 
+    ## Can be tailored to output the desired content
+    def appendList(self):
+        news_item = {
+            'symbol': self.keyword,
+            'title': self.title,
+            'text': self.body
+        }
+        news_list.append(news_item)
+
+
+## Website structure class
+## Parameters to be speficied by us in "sites" 
+class Website:
+    def __init__(self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag):
+        self.name = name
+        self.url = url
+        self.searchUrl = searchUrl
+        self.resultListing = resultListing
+        self.resultUrl = resultUrl
+        self.absoluteUrl = absoluteUrl
+        self.titleTag = titleTag
+        self.bodyTag = bodyTag
+
+
+## Crawler class 
+class Crawler:
+
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
+
+    def safeGet(self, pageObj, selector):
+        childObj = pageObj.select(selector)
+        if childObj is not None and len(childObj) > 0:
+            return childObj[0].get_text()
+        return ''
+
+    ##  Main search function that searches for a given keyword and records all pages returned 
+    def search(self, keyword, site):
+        bs = self.getPage(site.searchUrl + keyword) ## e.g. http://www.reuters.com/search/news?blob=MSFT
+        searchResults = bs.select(site.resultListing)
+        for result in searchResults:
+            url = result.select(site.resultUrl)[0].attrs['href'] ## Select the article urls 
+           
+            if(site.absoluteUrl):  # Check whether it's a relative or an absolute URL
+                bs = self.getPage(url)
+            else:
+                bs = self.getPage(site.url + url)
+            if bs is None:
+                print('Error with the URL or the page. ') 
+                return
+            title = self.safeGet(bs, site.titleTag) ## grab title 
+            body = self.safeGet(bs, site.bodyTag) ## grab body 
+            if title != '' and body != '':
+                content = Content(keyword, url, title, body) ## Structure the article content for output 
+                content.appendList()
+
+
+
+
+
+## Here to specify a list of websites along with relevant information for scraping 
+## Feel free to add as many sites as you want, but double check each parameter before running 
+## self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag
+sitesParam = [
+    ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=',
+    'div.search-result-content', 'h3.search-result-title a', False, 'h1',
+    'div.StandardArticleBody_body']
+]
+sites = []
+for row in sitesParam:
+    sites.append(Website(row[0], row[1], row[2],row[3], row[4], row[5], row[6], row[7]))
+
+
+crawler = Crawler()
+
+## Here to specify a query list (should be tickers) as keywords
+## e.g. ['ADOM', 'ASV', 'AESEW', 'EMMA']
+suspended = pd.read_csv('/Users/Frank.peng/Desktop/suspended_2019-09-25-18-39.csv')
+keywords = np.asarray(suspended['Symbol'])
+## keywords =['ADOM', 'ASV', 'AESEW', 'GOOGL', 'MSFT']
+
+for k in keywords:
+    print('Working on: ' + k + ' now')
+    for targetSite in sites:
+        crawler.search(k, targetSite)
+
+
+output_file = datetime.datetime.now().strftime("news_output_%Y-%m-%d-%H-%M.csv")
+df_news_output = pd.DataFrame(news_list)
+for e in news_list:
+    df_news_output.loc[len(df_news_output)] = e
+
+df_news_output.to_csv(output_file, encoding='utf-8', index=False)
+
---
+++ suspended_crawler.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import re
+import urllib
+import requests 
+
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd 
+import datetime
+
+DOWNLOAD_URL = 'https://listingcenter.nasdaq.com/IssuersPendingSuspensionDelisting.aspx'
+
+
+def download_page(url):
+    return requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+    }).text
+
+
+soup = BeautifulSoup(download_page(DOWNLOAD_URL),'lxml')
+stock_list_section = soup.find('body').find('form').find('div', attrs={'align': 'center'})
+
+output_file = datetime.datetime.now().strftime("suspended_%Y-%m-%d-%H-%M.csv")
+
+
+stock_master_wrapper = stock_list_section.find('div', attrs={'class': 'masterwrapper'})
+stock_main_container = stock_master_wrapper.find('div', attrs={'id': 'maincontainer'})
+tbl_master = stock_main_container.find("div", attrs={'id': 'ctl00_tblMaster'})
+stock_list = tbl_master.find('div', attrs={'id': 'grdView'})
+table = stock_list.find('table', attrs={'class':'rgMasterTable'})
+
+headers = ['Issuer Name','Symbol','Reason','Status','Effective Date','Form 25 Date']
+df = pd.DataFrame(columns=headers)
+
+stocks = []
+for row in table.find_all("tr", attrs = {"class" : "rgRow"}):
+    stocklist = []
+    for cell in row.find_all("td"):
+        stocklist.append(cell.text)
+    stocklist = [element.replace('\xa0', 'None') for element in stocklist]
+    stocks.append(stocklist)
+##  print(stocklist)
+
+for e in stocks:
+    df.loc[len(df)] = e
+
+df = df.loc[df['Status'] == 'Suspended']
+df.to_csv(output_file, encoding='utf-8', index=False)
